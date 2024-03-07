c54ec4d3e3041622611e194edab37f438fddbc2f
Lautaro Gonzalez <59624470+Lautaro124@users.noreply.github.com>
2024-03-07 16:27:07 -0300

    UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,26 +11,35 @@ export default function Home() {
     if (number >= 4000) {
       setRomanNumbers('El numero es mayor a 3000')
     }
+    else if (number <= 0) {
+      setRomanNumbers('El numero no puede ser 0 ni menor a tal')
+    }
     else {
       setRomanNumbers(convertor(number))
     }
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form onSubmit={handlerSubmit}>
-        <label htmlFor="number">
-          <input
-            type="number"
-            name="number"
-            className="text-black"
-          />
+    <main className="flex min-h-screen flex-col items-center justify-center p-8 gap-8">
+      <form
+        onSubmit={handlerSubmit}
+        className="flex flex-col gap-3"
+      >
+        <label htmlFor="number" className="text-lg">
+          Ingrese un número entero:
         </label>
-        <button>Convertir</button>
+        <input
+          type="number"
+          name="number"
+          className="text-black border rounded-md p-2"
+        />
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">
+          Convertir
+        </button>
       </form>
       <section>
-        <h3>Resultado: </h3>
-        <h1>{romanNumbers}</h1>
+        <h3 className="text-lg font-semibold">Resultado: </h3>
+        <h1 className="text-3xl">{romanNumbers}</h1>
       </section>
     </main>
   );
